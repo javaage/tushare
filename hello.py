6352a63b31fc6e75301f4bbac001dd81b68c022a
javaage <subing.xiao@pactera.com>
2018-02-11 11:11:45 +0800

    indent

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ from flask import Flask
 app=Flask(__name__)
 @app.route('/hello')
 def hello_world():
-	user_agent = request.headers.get('User-Agent')
+    user_agent = request.headers.get('User-Agent')
     return '<p>Your browser is %s</p>' % user_agent
 @app.route('/user/<name>')
 def user(name):
