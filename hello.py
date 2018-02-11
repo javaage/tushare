081480d1eff49870fc2e071f38e2ec3c417a5ed8
javaage <subing.xiao@pactera.com>
2018-02-11 11:04:57 +0800

    modify alittle

--- hello.py
+++ hello.py
@@ -2,6 +2,10 @@ from flask import Flask
 app=Flask(__name__)
 @app.route('/hello')
 def hello_world():
-    return "Hello World"
+	user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is %s</p>' % user_agent
+@app.route('/user/<name>')
+def user(name):
+	return '<h1>Hello, %s</h1>' % name
 if __name__=='__main__':
     app.run()
